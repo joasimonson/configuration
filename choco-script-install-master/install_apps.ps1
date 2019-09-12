bbcef39c6dfa4f9ac1a53812d719c75eb30ab791
Joasimonson Araújo <joasimonson@hotmail.com>
2019-09-11 22:00:14 -0300

    Add files via upload

---
+++ choco-script-install-master/install_apps.ps1
@@ -0,0 +1,91 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# Script de instala��o programas	#
+# script by Takehana, Thiago		#
+# v0.0.1 03/08/2019					#
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+#---------------------
+# DEV TOOLS
+#---------------------
+
+# Visual Studio Code
+choco install vscode -y
+choco install vscode-docker
+choco install vscode-gitlens
+
+# ILSPY decompiler
+choco install ilspy -y
+
+# SQL Management Studio
+choco install sql-server-management-studio -y
+
+# SVN Tortoise
+choco install tortoisesvn -y
+
+# Git
+choco install git -y
+choco install poshgit -y
+
+# Node js
+choco install nodejs -y
+
+# Yarn
+choco install yarn -y
+
+# NuGet Package Explorer
+choco install nugetpackageexplorer -y
+
+# WinSCP
+choco install winscp -y
+
+# Postman
+choco install postman -y
+
+# Docker
+choco install docker-desktop -y
+
+#-----------------------
+# Ferramentas auxiliares
+#-----------------------
+
+# Notepad++
+choco install notepadplusplus -y
+
+# Open VPN
+choco install openvpn -y
+
+# Foxit reader
+choco install foxitreader -y
+
+# 7 zip
+choco install 7zip -y
+
+# Google Chrome
+choco install googlechrome -y
+
+# Firefox
+choco install firefox -y
+
+# Dropbox
+choco install dropbox -y
+
+# Google Play Music Desktop (Unofficial)
+choco install gpmdp -y
+
+# Paint.net
+choco install paint.net -y
+
+# WindirStat (Explorador de arquivos)
+choco install windirstat -y
+
+# Office 365 proplus
+# todo configurar instalacao com apps customizado e em portugues
+
+# Microsoft teams
+choco install microsoft-teams -y
+
+# Logitech options
+choco install logitech-options -y
+
+# Process explorer
+choco install procexp -y
