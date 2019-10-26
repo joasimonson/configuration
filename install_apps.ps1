127567d274dad841abf5824a98956d426554bafa
Joasimonson Araújo <joasimonson@hotmail.com>
2019-10-26 09:33:42 -0300

    Add files via upload

---
+++ install_apps.ps1
@@ -0,0 +1,54 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# Script de instalação de programas	#
+# script by Araújo, Jo				#
+# v0.0.1 22/10/2019					#
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+#---------------------
+# Development
+#---------------------
+
+# Visual Studio Code
+choco install vscode -y
+choco install vscode-gitlens
+
+# ILSPY
+choco install ilspy -y
+
+# SQL Management Studio
+choco install sql-server-management-studio -y
+
+# Git
+choco install git -y
+choco install poshgit -y
+
+# Node js
+choco install nodejs -y
+
+# Postman
+choco install postman -y
+
+#-----------------------
+# Auxiliar
+#-----------------------
+
+# Notepad++
+choco install notepadplusplus -y
+
+# Foxit reader
+choco install foxitreader -y
+
+# 7 zip
+choco install 7zip -y
+
+# Google Chrome
+choco install googlechrome -y
+
+# Paint.net
+choco install paint.net -y
+
+# WindirStat (Análise de arquivos)
+choco install windirstat -y
+
+# Process explorer
+choco install procexp -y
