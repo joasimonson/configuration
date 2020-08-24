379479f56905ed4a91a09609d7f47de6871f41da
Joasimonson Araújo <joasimonson@hotmail.com>
2020-08-23 21:29:36 -0300

    Update install_apps.ps1

--- install_apps.ps1
+++ install_apps.ps1
@@ -1,65 +1,39 @@
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-# Script de instalação de programas	#
-# script by Araújo, Jo				#
-# v0.0.1 22/10/2019					#
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# Dev
 
-#---------------------
-# Development
-#---------------------
+# ExecutionPolicy
+Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
 
-# Visual Studio Code
+# Chocolatey Install
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install vscode -y
 choco install vscode-gitlens
 
-# ILSPY
 choco install ilspy -y
 
-# SQL Management Studio
-# choco install sql-server-management-studio -y
-choco install qbittorrent
+choco install azure-data-studio
 
-# Git
+choco install docker-desktop --version=2.0.0.3
+
 choco install git -y
 choco install poshgit -y
 
-# Node js
 choco install nodejs -y
 
-# Postman
 choco install postman -y
 
-#-----------------------
-# Auxiliar
-#-----------------------
+# Aux
 
-# Notepad++
 choco install notepadplusplus -y
-
-# Foxit reader
 choco install foxitreader -y
-
-# 7 zip
 choco install 7zip -y
-
-# Google Chrome
-choco install googlechrome -y
-
-# Paint.net
 choco install paint.net -y
+choco install windirstat -y # WindirStat (Análise de arquivos)
+choco install procexp -y # Process explorer
+choco install qbittorrent
 
-# WindirStat (Análise de arquivos)
-choco install windirstat -y
-
-# Process explorer
-choco install procexp -y
-
-
-# Programas inutilizados Windows
+# Remove windows programs default
 remove-AppxPackage Microsoft.ZuneMusic_10.19031.11411.0_x64__8wekyb3d8bbwe
 remove-AppxPackage Microsoft.WindowsMaps_5.1906.1972.0_x64__8wekyb3d8bbwe
 remove-AppxPackage Microsoft.Messaging_4.1901.10241.1000_x64__8wekyb3d8bbwe
 remove-AppxPackage Microsoft.People_10.1902.633.0_x64__8wekyb3d8bbwe
-
-# ExecutionPolicy
-Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
