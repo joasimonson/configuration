1dc42f99d5281c5c79a5cb85b277271aa76cfe66
Joasimonson Araújo <joasimonson@hotmail.com>
2021-06-24 19:07:52 -0300

    Update install_apps.ps1

--- install_apps.ps1
+++ install_apps.ps1
@@ -5,11 +5,11 @@ Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Dev
-choco install powershell-core
+choco install powershell-core -y
 choco install vscode -y
 choco install ilspy -y
 choco install postman -y
-choco install azure-data-studio
+choco install azure-data-studio -y
 
 choco install docker-desktop -y
 
