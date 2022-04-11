393052e3f6afbc2ec41a59c4688bf3b17a479165
joasimonson <joasimonson@hotmail.com>
2022-04-11 15:21:22 -0300

    Update install_apps.ps1

--- install_apps.ps1
+++ install_apps.ps1
@@ -6,19 +6,35 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 ## chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-winget source add --accept-source-agreements --name msstore https://storeedgefd.dsx.mp.microsoft.com/v9.0
+# winget source add --accept-source-agreements --name msstore https://storeedgefd.dsx.mp.microsoft.com/v9.0
+winget source add --accept-source-agreements --name msstore https://winget.azureedge.net/msstore
 winget source add --accept-source-agreements --name winget https://winget.azureedge.net/cache
 
 winget uninstall cortana
 winget uninstall --id Microsoft.OneDrive
-winget uninstall --id Microsoft.WindowsNotepad_8wekyb3d8bbwe
 winget uninstall --id Microsoft.OneDriveSync_8wekyb3d8bbwe
-winget uninstall --id Microsoft.People_8wekyb3d8bbwe
-winget uninstall --id Microsoft.GetHelp_8wekyb3d8bbwe
-winget uninstall --id Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe
-winget uninstall --id Microsoft.WindowsMap_8wekyb3d8bbwe
-winget uninstall --id Microsoft.ZuneMusic_8wekyb3d8bbwe
 winget uninstall --id Microsoft.Messaging_8wekyb3d8bbwe
+winget uninstall --id Microsoft.BingNews_8wekyb3d8bbwe
+winget uninstall --id Microsoft.BingWeather_8wekyb3d8bbwe
+winget uninstall --id Microsoft.GamingApp_8wekyb3d8bbwe
+winget uninstall --id Microsoft.GetHelp_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Getstarted_8wekyb3d8bbwe
+winget uninstall --id Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+winget uninstall --id Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
+winget uninstall --id Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe
+winget uninstall --id Microsoft.People_8wekyb3d8bbwe
+winget uninstall --id Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Todos_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsAlarms_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsMaps_8wekyb3d8bbwe
+winget uninstall --id Microsoft.WindowsNotepad_8wekyb3d8bbwe
+winget uninstall --id Microsoft.Xbox.TCUI_8wekyb3d8bbwe
+winget uninstall --id Microsoft.XboxIdentityProvider_8wekyb3d8bbwe
+winget uninstall --id Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe
+winget uninstall --id Microsoft.ZuneMusic_8wekyb3d8bbwe
+winget uninstall --id Microsoft.ZuneVideo_8wekyb3d8bbwe
+winget uninstall --id microsoft.windowscommunicationsapps_8wekyb3d8bbwe
 
 del "$env:SystemRoot\notepad.exe"
 del "$env:SystemRoot\System32\notepad.exe" # permissions needed
